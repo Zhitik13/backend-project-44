655b202f74e6f52c606a8c8a3b409f9fe3328b21
Zhitik13 <94741933+Zhitik13@users.noreply.github.com>
2022-10-09 09:18:13 +0400

    delete function data

--- src/utilits.js
+++ src/utilits.js
@@ -1,6 +1,3 @@
 export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-export function randomIndex(data) {
-  return Math.floor(Math.random() * data.length);
-}
